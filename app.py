a3717cbeaa2c0f7e77bd427473e5518c66ddfb10
Justin Furlotte <justinfurlotte@justins-mbp.home>
2025-03-20 21:17:47 -0300

    curly brackets

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if uploaded_file:
                                f"Column {col}\n"
                                f"Contains student {student}. Please verify there is a space, a dash, "
                                f"and a space, e.g. ' - ', and not ' -' or '- ' or '-'.")
-                    st.write(f"message\n\nError: {e}")
+                    st.write(f"{message}\n\nError: {e}")
                     raise ValueError(e)
                 sub_df = pd.DataFrame({"Class": [student_class], "Student": [student_name], "WIN Block": [col]})
                 blocks.append(sub_df)
